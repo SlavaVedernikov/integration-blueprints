07868beb995145f10cee04136f82dcb7d799770b
Slava Vedernikov <82385901+SlavaVedernikovNL@users.noreply.github.com>
2023-01-12 22:59:46 +0000

    removed use of zip_deflate

--- js/index.js
+++ js/index.js
@@ -37,7 +37,7 @@
 
         pumlDiagram.replace("<<Content>>", blocks.join("\n"));
 
-        $("#diagram").attr("src", "http://www.plantuml.com/plantuml/img/"+encode64(zip_deflate(pumlDiagram, 9)));
+        $("#diagram").attr("src", "http://www.plantuml.com/plantuml/img/"+encode64(pumlDiagram));
     }
 
     encode64 = function(data) {
