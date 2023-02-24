fed473d805c4ef864cb62906f8a3132eec4bd757
Slava Vedernikov <82385901+SlavaVedernikovNL@users.noreply.github.com>
2023-02-24 16:12:28 +0000

    Added blueprint comments and page meta

--- js/index.js
+++ js/index.js
@@ -20,8 +20,218 @@
         ["consumer-file-transfer", "consumer-shared-db", "consumer-rpc-pull", "consumer-rpc-push", "consumer-messaging", "consumer-webhook"]
     ];
 
+    var externalLinksMapping = [
+        {
+            token: "TRANSLATOR",
+            link: "https://www.enterpriseintegrationpatterns.com/patterns/messaging/MessageTranslator.html",
+            text: "Translator"
+        },
+        {
+            token: "CANONICAL",
+            link: "https://www.enterpriseintegrationpatterns.com/patterns/messaging/CanonicalDataModel.html",
+            text: "Canonical"
+        }
+    ];
+
+    var numberingUnicodeMapping = [
+        {
+            token: "1",
+            valueUnicode: "U+2776",
+            valueHtml: "&#x2776;"
+        },{
+            token: "2",
+            valueUnicode: "U+2777",
+            valueHtml: "&#x2777;"
+        },{
+            token: "3",
+            valueUnicode: "U+2778",
+            valueHtml: "&#x2778;"
+        },{
+            token: "4",
+            valueUnicode: "U+2779",
+            valueHtml: "&#x2779;"
+        },{
+            token: "5",
+            valueUnicode: "U+277A",
+            valueHtml: "&#x277A;"
+        },{
+            token: "6",
+            valueUnicode: "U+277B",
+            valueHtml: "&#x277B;"
+        },{
+            token: "7",
+            valueUnicode: "U+277C",
+            valueHtml: "&#x277C;"
+        },{
+            token: "8",
+            valueUnicode: "U+277E",
+            valueHtml: "&#x277E;"
+        },{
+            token: "9",
+            valueUnicode: "U+277E",
+            valueHtml: "&#x277E;"
+        },{
+            token: "10",
+            valueUnicode: "U+277F",
+            valueHtml: "&#x277F;"
+        },{
+            token: "11",
+            valueUnicode: "U+24EB",
+            valueHtml: "&#x24EB;"
+        },{
+            token: "12",
+            valueUnicode: "U+24EC",
+            valueHtml: "&#x24EC;"
+        },{
+            token: "13",
+            valueUnicode: "U+24ED",
+            valueHtml: "&#x24ED;"
+        },{
+            token: "14",
+            valueUnicode: "U+24EE",
+            valueHtml: "&#x24EE;"
+        },{
+            token: "15",
+            valueUnicode: "U+24EF",
+            valueHtml: "&#x24EF;"
+        },{
+            token: "16",
+            valueUnicode: "U+24F0",
+            valueHtml: "&#x24F0;"
+        },{
+            token: "17",
+            valueUnicode: "U+24F1",
+            valueHtml: "&#x24F1;"
+        },{
+            token: "18",
+            valueUnicode: "U+24F2",
+            valueHtml: "&#x24F2;"
+        },{
+            token: "19",
+            valueUnicode: "U+24F3",
+            valueHtml: "&#x24F3;"
+        },{
+            token: "20",
+            valueUnicode: "U+24F4",
+            valueHtml: "&#x24F4;"
+        }
+    ];
+
+    var urlSegmentMetaMapping = {
+        templates: [{
+            key: "title",
+            value: "Integration Blueprint: <<PRODUCER_CONTRACT_TYPE>> Producer <<PRODUCER_INTEGRATION_STYLE>>, <<CONSUMER_CONTRACT_TYPE>> Consumer - <<CONSUMER_INTEGRATION_STYLE>>"
+        },{
+            key: "description",
+            value: "Producer is <<PRODUCER_CONTRACT_TYPE>> in relation to Canonical Data Exchange Contract. <<PRODUCER_INTEGRATION_STYLE>> is a prefered integration style of a Producer. Consumer is <<CONSUMER_CONTRACT_TYPE>> in relation to Canonical Data Exchange Contract. <<CONSUMER_INTEGRATION_STYLE>>  is a prefered integration style of a Consumer."
+        },
+        {
+            key: "description",
+            urlSegment: "p-adapter",
+            value: "Producer Adapter is used to enable data exchange between Producer and Mediation Layer. "
+        },
+        {
+            key: "description",
+            urlSegment: "p-api",
+            value: "Producer API is used as a facade between Producer and/or Producer Adapter and the Mediation Layer."
+        },
+        {
+            key: "description",
+            urlSegment: "c-adapter",
+            value: "Consumer Adapter is used to enable data exchange between the Mediation Layer and Consumer. "
+        },
+        {
+            key: "description",
+            urlSegment: "c-api",
+            value: "Consumer API is used as a facade between Consumer and/or Consumer Adapter and the Mediation Layer."
+        }],
+        mappings: [{
+            urlSegment: "producer-file-transfer",
+            token: "PRODUCER_INTEGRATION_STYLE",
+            value: "File Transfer"
+        },
+        {
+            urlSegment: "producer-messaging",
+            token: "PRODUCER_INTEGRATION_STYLE",
+            value: "Messaging"
+        },
+        {
+            urlSegment: "producer-shared-db",
+            token: "PRODUCER_INTEGRATION_STYLE",
+            value: "Shared Database"
+        },
+        {
+            urlSegment: "producer-rpc-pull",
+            token: "PRODUCER_INTEGRATION_STYLE",
+            value: "Remote Procedure Call (RPC)"
+        },
+        {
+            urlSegment: "producer-rpc-push",
+            token: "PRODUCER_INTEGRATION_STYLE",
+            value: "Remote Procedure Call (RPC)"
+        },
+        {
+            urlSegment: "producer-webhook",
+            token: "PRODUCER_INTEGRATION_STYLE",
+            value: "Webhook"
+        },
+        {
+            urlSegment: "p-conformist",
+            token: "PRODUCER_CONTRACT_TYPE",
+            value: "Conformist"
+        },
+        {
+            urlSegment: "p-non-conformist",
+            token: "PRODUCER_CONTRACT_TYPE",
+            value: "Non-Conformist"
+        },{
+            urlSegment: "consumer-file-transfer",
+            token: "CONSUMER_INTEGRATION_STYLE",
+            value: "File Transfer"
+        },
+        {
+            urlSegment: "consumer-messaging",
+            token: "CONSUMER_INTEGRATION_STYLE",
+            value: "Messaging"
+        },
+        {
+            urlSegment: "consumer-shared-db",
+            token: "CONSUMER_INTEGRATION_STYLE",
+            value: "Shared Database"
+        },
+        {
+            urlSegment: "consumer-rpc-pull",
+            token: "CONSUMER_INTEGRATION_STYLE",
+            value: "Remote Procedure Call (RPC)"
+        },
+        {
+            urlSegment: "consumer-rpc-push",
+            token: "CONSUMER_INTEGRATION_STYLE",
+            value: "Remote Procedure Call (RPC)"
+        },
+        {
+            urlSegment: "consumer-webhook",
+            token: "CONSUMER_INTEGRATION_STYLE",
+            value: "Webhook"
+        },
+        {
+            urlSegment: "c-conformist",
+            token: "CONSUMER_CONTRACT_TYPE",
+            value: "Conformist"
+        },
+        {
+            urlSegment: "c-non-conformist",
+            token: "CONSUMER_CONTRACT_TYPE",
+            value: "Non-Conformist"
+        }]
+    };
+
     var urlSegmentPlantUmlTokenMapping = [
         {
+            urlSegment: "producer-file-transfer",
+            token: "USE_PRODUCER_CHANNEL",
+            value: "true"
+        },{
             urlSegment: "producer-webhook",
             token: "USE_PRODUCER_ADAPTER_CHANNEL",
             value: "true"
@@ -173,23 +383,107 @@
                         url: "../../plantuml/" + blocks[i].key +".puml",
                         type: 'GET',
                         success: function(data) {     
-                            blocks[index].value = replacePlantUmlTokens(data, blueprintComponents);
-                            if(blocks.filter(x => x.value == undefined) == 0) {
+                            blocks[index].puml = replacePlantUmlTokens(data, blueprintComponents);
+                            if(blocks.filter(x => x.puml == undefined) == 0) {
                                 renderBlueprint(blocks);
                             }
                         }
                     });
              })(i);
-        }  
+
+             (function(index){
+                $.ajax(
+                    {
+                        url: "../../plantuml/" + blocks[i].key +".json",
+                        type: 'GET',
+                        success: function(data) {     
+                            blocks[index].comments = getPlantUmlComments(data, blueprintComponents);
+                            if(blocks.filter(x => x.comments == undefined) == 0) {
+                                renderComments(blocks);
+                            }
+                        }
+                    });
+             })(i);
+        } 
+        
+        setMetaTags(blueprintComponents);
+    };
+
+    renderComments = function(blocks) {
+        if(!blocks) return;
+        var markdownToHtmlConverter = new showdown.Converter();
+        blocks.forEach(block => {
+            if(!block.comments) return;
+            block.comments.forEach(comment => {
+                $( "#diagram-comments" ).append( "<h2>" + comment.component + "</h2>" );
+                comment.values.forEach(value => {
+                    $( "#diagram-comments" ).append(markdownToHtmlConverter.makeHtml(getHtmlNumbering(value.index) + " " + formatExternalLinks(value.comment) + formatNote(value.note)) );
+                });
+            });
+        });
+    };
+
+    formatExternalLinks = function(text) {
+        externalLinksMapping.forEach(mapping => {
+            text = text.replace(new RegExp("<<" + mapping.token + ">>","g"), "<a href=\"" + mapping.link + "\" target=\"_blank\">" + mapping.text + "</a>");
+        });
+
+        return text;
+    };
+
+    formatNote = function(note) {
+        if(!note) return "";
+        return "<p>\n```md\nNOTE: " + note + "\n```<p/>";
+    };
+
+    getHtmlNumbering = function(number) {
+        var result = number;
+        var mapping = numberingUnicodeMapping.find(x => x.token == number);
+
+        if(mapping)
+        {
+            result = mapping.valueHtml;
+        }
+        
+        return result;
     };
 
     renderBlueprint = function(blocks) {
-        var pumlDiagram = blocks.find(x => x.key == "template").value;
+        var pumlDiagram = blocks.find(x => x.key == "template").puml;
         blocks = blocks.filter(x => x.key !== "template");
 
-        pumlDiagram = pumlDiagram.replace("<<CONTENT>>", blocks.map(x => x.value).join('\n\n'));
+        pumlDiagram = pumlDiagram.replace("<<CONTENT>>", blocks.map(x => x.puml).join('\n\n'));
 
         $("#diagram").attr("src", "http://www.plantuml.com/plantuml/img/" + window.plantumlEncoder.encode(pumlDiagram));
+    };
+
+    getPlantUmlComments = function(commentsConfig, components) {
+        var result = []
+        var mappings = [];
+
+        components.forEach(component => {
+            mappings = mappings.concat(urlSegmentPlantUmlTokenMapping.filter(mapping => mapping.urlSegment == component));
+        });
+
+
+        commentsConfig.data.forEach(data => {
+            result.push({component: data.component, values: filterComments(data.comments, mappings, [])});
+        });
+
+        return result;
+    };
+
+    filterComments = function(comments, mappings, result) {
+        var data = comments.find(x => mappings.some(y => y.token == x.condition.token && y.value == x.condition.value));
+        if(data)
+        {
+            result = result.concat(data.values);
+            if(data.comments)
+            {
+                result =  filterComments(data.comments, mappings, result);
+            }
+        }
+        return result;
     };
 
     replacePlantUmlTokens = function(plantUml, components) {
@@ -198,12 +492,36 @@
             var mappings = urlSegmentPlantUmlTokenMapping.filter(mapping => mapping.urlSegment == component);
 
             mappings.forEach(mapping => {
-                plantUml = plantUml.replace("<<" + mapping.token + ">>", mapping.value);
+                plantUml = plantUml.replace(new RegExp("<<" + mapping.token + ">>", "g"), mapping.value);
+            });
+
+            numberingUnicodeMapping.forEach(mapping => {
+                plantUml = plantUml.replace(new RegExp("<<" + mapping.token + ">>", "g"), "<" + mapping.valueUnicode + ">");
             });
         });
 
         return plantUml;
-    }
+    };
+
+    setMetaTags = function(components) {
+
+        var title = urlSegmentMetaMapping.templates.find(x => x.key == "title").value;
+        var descriptions = urlSegmentMetaMapping.templates.filter(x => x.key == "description");
+
+        components.forEach(component => {
+            var mappings = urlSegmentMetaMapping.mappings.filter(mapping => mapping.urlSegment == component);
+
+            mappings.forEach(mapping => {
+                title = title.replace(new RegExp("<<" + mapping.token + ">>", "g"), mapping.value);
+                descriptions.forEach(description => {
+                    description.value = description.value.replace(new RegExp("<<" + mapping.token + ">>", "g"), mapping.value);
+                });
+            });
+        });
+
+        $("#meta-description").attr("content", descriptions.map(x => x.value).join(" "));
+        $(document).attr("title", title);
+    };
 
     updateLinks = function() {
         var blueprintCode = window.location.pathname.replace("index.html", "").split("/").filter(x => x).pop();
